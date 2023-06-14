9f814d5966e8c62ce6f8c19c1080c2d134d27906
Ladislav Sedláček <sedlacek.lada@gmail.com>
2023-06-14 14:44:34 +0200

    Showroom, competition and how to sections finished

---
+++ js/f__svg-sprite.js
@@ -0,0 +1,12 @@
+const sprite_engine = () => {
+    fetch('https://cdn.alza.cz/Foto/or/lp/lego-sis/build/img/sprite.svg?ver=1.02')
+    .then(data => data.text())
+    .then(text => {
+        const sprite_element = document.createElement("div")
+        const landingpage_target = document.querySelector('#landingpage')
+        sprite_element.innerHTML = text
+        landingpage_target.append(sprite_element);
+        landingpage_target.lastElementChild.classList.add("svgsprite")
+    })
+}
+sprite_engine()
